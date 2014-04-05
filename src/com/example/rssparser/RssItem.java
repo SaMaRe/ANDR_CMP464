9b3c427e8d8095c27c44fef5bc04ad3134de97e6
SaMaRe <amaet_12@hotmail.com->
2014-04-05 13:34:31 -0400

    Homework4

---
+++ src/com/example/rssparser/RssItem.java
@@ -0,0 +1,46 @@
+package com.example.rssparser;
+
+public class RssItem {
+
+    private String Title;
+    private String Link;
+    private String Description;
+
+
+    public RssItem() {
+        Title = "";
+        Link = "";
+        Description = "";
+    }
+
+    public RssItem(String title, String link, String description) {
+        Title = title;
+        Link = link;
+        Description = description;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getLink() {
+        return Link;
+    }
+
+    public void setLink(String link) {
+        Link = link;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+}
